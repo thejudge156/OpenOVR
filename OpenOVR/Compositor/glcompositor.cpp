edd5e886ad9d6627dfe8c949144b4767128a6d11
The Judge <53906078+thejudge156@users.noreply.github.com>
2022-11-28 19:12:39 -0800

    Forgot to remove this from here

--- OpenOVR/Compositor/glcompositor.cpp
+++ OpenOVR/Compositor/glcompositor.cpp
@@ -82,8 +82,8 @@ void GLBaseCompositor::Invoke(const vr::Texture_t* texture, const vr::VRTextureB
 	// Calculate how large the area to copy is
 	GLsizei inputWidth, inputHeight, rawFormat;
 	glBindTexture(GL_TEXTURE_2D, src); // Sadly even GLES3.2 doesn't have glGetTextureLevelParameteriv which takes the image directly
-	inputWidth = 1440;
-	inputHeight = 1584;
+	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_WIDTH, &inputWidth);
+    glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_HEIGHT, &inputHeight);
 	glGetTexLevelParameteriv(GL_TEXTURE_2D, 0, GL_TEXTURE_INTERNAL_FORMAT, &rawFormat);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	XrRect2Di viewport;
@@ -146,7 +146,6 @@ void GLBaseCompositor::Invoke(const vr::Texture_t* texture, const vr::VRTextureB
 		// Just warn, so we can find it in the logs - I fear it may crash in unexpected corner-cases otherwise.
 		// OOVR_ABORTF("OpenGL texture copy failed with err %d", err);
 		OOVR_LOG_ONCE("WARNING: OpenGL texture copy failed!");
-		printf("%d", err);
 	}
 
 	// Release the swapchain - OpenXR will use the last-released image in a swapchain
